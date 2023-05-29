7be60467eb5e5eadfc4e84e082c43f27ffcd9bf8
Bliss  Armah-Nwanwah <blissarmah8@gmail.com>
2023-05-29 12:20:24 +0000

    security packages changed

--- app.js
+++ app.js
@@ -38,10 +38,6 @@ app.use(xss())
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
-app.get('*',(req,res)=>{
-  res.sendFile(path.resolve(__dirname,'./client/build','index.html'))
-})
-
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
